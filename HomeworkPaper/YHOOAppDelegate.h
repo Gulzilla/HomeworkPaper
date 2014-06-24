e59b71a32a0a3b522bc2e19812cc1a8608120abe
tgulik <tgulik@yahoo-inc.com>
2014-06-23 22:29:04 -0700

    Initial Commit

---
+++ HomeworkPaper/YHOOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YHOOAppDelegate.h
+//  HomeworkPaper
+//
+//  Created by Tom Gulik on 6/23/14.
+//  Copyright (c) 2014 Tom Gulik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHOOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomeworkPaper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HomeworkPaper
+//
+//  Created by Tom Gulik on 6/23/14.
+//  Copyright (c) 2014 Tom Gulik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YHOOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YHOOAppDelegate class]));
+    }
+}
